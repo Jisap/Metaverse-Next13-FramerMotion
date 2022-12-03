5376edfae49ee54324bdb169673ae33bd98dd8fc
Jorge Matos <jisapjisap@gmail.com>
2022-12-03 11:45:49 +0100

    Primer commit

---
+++ utils/motion.js
@@ -0,0 +1,161 @@
+export const navVariants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 80,
+      delay: 1,
+    },
+  },
+};
+
+export const slideIn = (direction, type, delay, duration) => ({
+  hidden: {
+    x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
+    y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
+
+export const staggerContainer = (staggerChildren, delayChildren) => ({
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren,
+      delayChildren,
+    },
+  },
+});
+
+export const textVariant = (delay) => ({
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 1.25,
+      delay,
+    },
+  },
+});
+
+export const textContainer = {
+  hidden: {
+    opacity: 0,
+  },
+  show: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
+  }),
+};
+
+export const textVariant2 = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'tween',
+      ease: 'easeIn',
+    },
+  },
+};
+
+export const fadeIn = (direction, type, delay, duration) => ({ // Las Cards entran a la derecha desde la izda
+  hidden: {//Si la dirección fuera izda la x=100
+    x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0, // Para valor dcha la x tiene un valor de -100 (esta fuera del borde la pantalla en el margen izdo)
+    y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+    opacity: 0,
+  },
+  show: {
+    x: 0, // Cuando se muestran las Cards el valor de la se restablece a valor normal x=0
+    y: 0,
+    opacity: 1,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
+
+export const planetVariants = (direction) => ({
+  hidden: {
+    x: direction === 'left' ? '-100%' : '100%',// Si la dirección es izda x=-100% del vw (fuera de la pantalla por la izda) y si es derecha lo contrario
+    rotate: 120,
+  },
+  show: {
+    x: 0,
+    rotate: 0,
+    transition: {
+      type: 'spring',
+      duration: 1.8,
+      delay: 0.5,
+    },
+  },
+});
+
+export const zoomIn = (delay, duration) => ({
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: 'tween',
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
+
+export const footerVariants = { // La animación se hace en el eje y (vertical) y=50 está abajo de la pantalla
+  hidden: {
+    opacity: 0,
+    y: 50,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 80,
+      delay: 0.5,
+    },
+  },
+};
